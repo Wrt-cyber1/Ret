c9f1b3c51d0646ec6e940aa9b103756f58d58710
Wrt-cyber1 <kakkakakatg@gmail.com>
2026-03-08 23:08:38 -0300

    Criar posts.js

---
+++ posts.js
@@ -0,0 +1,27 @@
+const posts = [
+
+{
+usuario:"fekes01",
+foto:"https://picsum.photos/500/300?1",
+texto:"Primeiro post na rede"
+},
+
+{
+usuario:"fekes02",
+foto:"https://picsum.photos/500/300?2",
+texto:"Curtindo a rede social"
+},
+
+{
+usuario:"fekes03",
+foto:"https://picsum.photos/500/300?3",
+texto:"Foto aleatória"
+},
+
+{
+usuario:"fekes04",
+foto:"https://picsum.photos/500/300?4",
+texto:"Mais um dia online"
+}
+
+]
