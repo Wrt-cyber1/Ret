aca12454111168bdbab63bfc416ef2b11bc6fb3e
Wrt-cyber1 <kakkakakatg@gmail.com>
2026-03-08 23:08:15 -0300

    Criar perfis.js

---
+++ perfis.js
@@ -0,0 +1,38 @@
+const perfis = [
+
+{
+nome:"fekes01",
+foto:"https://i.pravatar.cc/150?img=1",
+bio:"Explorando a rede",
+seguidores:124
+},
+
+{
+nome:"fekes02",
+foto:"https://i.pravatar.cc/150?img=2",
+bio:"Fotos e momentos",
+seguidores:532
+},
+
+{
+nome:"fekes03",
+foto:"https://i.pravatar.cc/150?img=3",
+bio:"Vida digital",
+seguidores:210
+},
+
+{
+nome:"fekes04",
+foto:"https://i.pravatar.cc/150?img=4",
+bio:"Postando sempre",
+seguidores:98
+},
+
+{
+nome:"fekes05",
+foto:"https://i.pravatar.cc/150?img=5",
+bio:"Internet vibes",
+seguidores:876
+}
+
+]
